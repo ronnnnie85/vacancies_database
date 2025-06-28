117872f44aff878df2d614499221d92e0a2ef473
ronnnnie85 <ronnnnie85@gmail.com>
2025-06-29 00:23:19 +0700

    создание и заполнение бд

---
+++ src/db_manager.py
@@ -0,0 +1,26 @@
+class DBManager:
+
+    __database_name: str
+
+    def __init__(self, database_name: str) -> None:
+        self.__database_name = database_name
+
+
+    def get_companies_and_vacancies_count(self) -> list:
+        pass
+
+
+    def get_all_vacancies(self) -> list:
+        pass
+
+
+    def get_avg_salary(self) -> float:
+        pass
+
+
+    def get_vacancies_with_higher_salary(self) -> list:
+        pass
+
+
+    def get_vacancies_with_keyword(self, keyword: str) -> list:
+        pass
